5249f753543178aeae362120b4eb39f4a07af5cd
Jaheed Haynes <jaheedhaynes@Jaheeds-MacBook-Pro.local>
2019-10-04 01:27:41 -0400

    update tba

--- Get-Home-TBA/Get-Home-TBA/main.swift
+++ Get-Home-TBA/Get-Home-TBA/main.swift
@@ -5,7 +5,18 @@
 //  Created by Jaheed Haynes on 10/3/19.
 //  Copyright © 2019 Jaheed Haynes. All rights reserved.
 //
+/*default:
+print()
+print("ARE YOU TRYING TO GET HOME!?!?! 'YES' or 'NO' ")
+print()
+print("TIME IS WASTING...YOU NEED TO GET HOME?!?")
+ // } while response == "NO"
+  // print("AwWWW MAN, HERE WE GO AGAIN")
 
+ //if response == "yes" {
+ //  print("About Time Man! Let's Get home A$AP!")
+ //}
+ */
 import Foundation
 
 
@@ -22,7 +33,7 @@ print("YOU'RE TIRED 😴😴😴")
 sleep(3)
 print()
 
-print("YOU'RE STINK 🤮🤮🤮")
+print("YOU STINK 🤮🤮🤮")
 sleep(3)
 print()
 
@@ -34,3 +45,112 @@ print("CHOOSE YOUR PATH HOME WISELY...")
 sleep(3)
 print()
 
+var response = ""
+
+repeat {
+    print("Are you ready to go home? type 'YES' or 'NO'")
+    response = readLine()?.lowercased() ?? ""
+    switch response {
+    case "yes":
+        print("About Time Man! Let's Get home A$AP!")
+    //response = "yes"
+    default:
+        print()
+        print("YOU GOTTA BE KIDDING ME, YOU HAVE A HOME...GO TO IT!")
+        print()
+        print("ARE YOU READY TO LEAVE NOW?!?!?")
+    }
+    response = readLine() ?? ""
+} while response == "no"
+print("ARE YOU READY TO LEAVE NOW?!?!?")
+
+print()
+print("IT'S 3AM, WHICH PATH DO YOU WANT TO TAKE BACK HOME???")
+
+
+repeat{
+    print("'SUBWAY' or 'RUN'")
+    response = readLine() ?? " "
+    switch response {
+    case "RUN":
+        print()
+        print("YOU'RE RUNNING FOR ABOUT A MILE AND YOU GET TIRED")
+        sleep(2)
+        print("DO YOU WANT TO TAKE A BREAK?")
+        sleep(2)
+        print("'YES' or 'NO'")
+        var response2 = " "
+    default:
+        print("ANSWER 'YES' or 'NO'")
+        
+        var response2 = " "
+        response2 = readLine() ?? " "
+        switch response2 {
+        case "YES":
+            print("WHILE YOU ARE RESTING A MYSTERIOUS MAN APPROACHES YOU AND ASKS FOR THE TIME")
+            sleep(3)
+            print("YOU TELL HIM ITS 3:30AM")
+            sleep(3)
+            print("THE MYSTERY MAN IS GREATFUL & PAYS FOR AN UBER FOR YOUR RIDE HOME 🚙💨")
+        case "NO":
+            print("YOU RUN AWAY AS FAST AS POSSIBLE")
+            sleep(3)
+            print("YOU MAKE IT HOME 2 HOURS LATER")
+            sleep(3)
+            print("YOU FORGOT YOUR KEYS AT PURSUIT!")
+            sleep(3)
+            print("☠️☠️☠️ YOU DIE OF HUNGER ☠️☠️☠️")
+        default:
+            print()
+            print("YOU GOTTA CHOOSE, 'YES' or 'NO' ")
+            print()
+            print("TIME IS WASTING...YOU NEED TO GET HOME?!?")
+        }
+        response2 = readLine() ?? ""
+    } while  response2 == "NO"
+    print("DON'T YOU WANT TO SLEEP IN YOUR BED? , EAT FOOD? , SHOWER!?!?!")
+} while response == "NO"
+print("AwWWW MAN, HERE WE GO AGAIN")
+    
+ 
+  /*  var response2 = " "
+    response2 = readLine() ?? " "
+    switch response2 {
+    case "YES":
+        print("WHILE YOU ARE RESTING A MYSTERIOUS MAN APPROACHES YOU AND ASKS FOR THE TIME")
+        sleep(3)
+        print("YOU TELL HIM ITS 3:30AM")
+        sleep(3)
+        print("THE MYSTERY MAN IS GREATFUL & PAYS FOR AN UBER FOR YOUR RIDE HOME 🚙💨")
+    case "NO":
+        print("YOU RUN AWAY AS FAST AS POSSIBLE")
+        sleep(3)
+        print("YOU MAKE IT HOME 2 HOURS LATER")
+        sleep(3)
+        print("YOU FORGOT YOUR KEYS AT PURSUIT!")
+        sleep(3)
+        print("☠️☠️☠️ YOU DIE OF HUNGER ☠️☠️☠️")
+    default:
+        print()
+        print("YOU GOTTA CHOOSE, 'YES' or 'NO' ")
+        print()
+        print("TIME IS WASTING...YOU NEED TO GET HOME?!?")
+    }
+    response2 = readLine() ?? ""
+} while response == "NO"
+print("DON'T YOU WANT TO SLEEP IN YOUR BED? , EAT FOOD? , SHOWER!?!?!")
+
+
+
+/*     response = "open door"
+ case "leave":
+ print(" ")
+ print("So we're just walking into big wooden objects:door:? you're dead! Try again!")
+ print(" ")
+ default:
+ print(" ")
+ print("Ugh! First you have to 'leave'. Come on!")
+ print("try again, maybe I don't know an 'open door' could help, but what do I know, right?")
+ print(" ")
+ }
+ */*/
